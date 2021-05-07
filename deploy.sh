73ad85f78432153a2c90a4d8aa1162a1c797e0fa
deno <inbox@email.com>
2021-05-07 11:06:57 +0800

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://mirrors.aliyun.com/composer/composer.phar -O /usr/local/bin/composer
+chmod a+x /usr/local/bin/composer
+composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+composer install -o --no-dev
+chown -R www:www bootstrap/cache storage
